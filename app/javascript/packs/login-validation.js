1d074e9544dc104512f98c4ebcbd628aac5c91d6
David Elì <davidelimg1@gmail.com>
2020-06-09 14:41:45 -0500

    Added validation for signup form

--- app/javascript/packs/login-validation.js
+++ app/javascript/packs/login-validation.js
@@ -1,6 +1,7 @@
 const FormChecker = (() => {
   const email = document.getElementById('user_email');
   const password = document.getElementById('user_password');
+  const passwordConfirmation = document.getElementById('user_password_confirmation');
   const alertBox = document.getElementById('alert-content');
 
 
@@ -10,44 +11,61 @@ const FormChecker = (() => {
       scanKey(email, 'Email');
       scanFocus(password, 'Password');
       scanKey(password, 'Password');
+      scanFocus(passwordConfirmation, 'PassConfirm');
+      scanKey(passwordConfirmation, 'PassConfirm');
     } else {
       scanItem(email, 'Email');
       scanKey(email, 'Email');
       scanItem(password, 'Password');
       scanKey(password, 'Password');
+      scanFocus(passwordConfirmation, 'PassConfirm');
+      scanKey(passwordConfirmation, 'PassConfirm');
     }
   };
 
   const scanFocus = (item, itemStr) => {
-    item.addEventListener('focus', (e) => {
-      scanItem(item, itemStr);
-    });
+    if (item != null) {
+      item.addEventListener('focus', (e) => {
+        scanItem(item, itemStr);
+      });
+    }
   };
 
   const scanKey = (item, itemStr) => {
-    item.addEventListener('keydown', (e) => {
-      scanItem(item, itemStr);
-    });
+    if (item != null) {
+      item.addEventListener('keyup', (e) => {
+        scanItem(item, itemStr);
+      });
+    }
   };
 
   const scanItem = (item, itemStr) => {
     const value = item.value.trim();
+    const pass = password.value.trim();
+
     switch(itemStr) {
       case 'Email':
-        if(value === '') {
+        if(value.length <= 0) {
           setErrorFor(item, `${itemStr} cannot be blank`);
-        }
-        else if (!isEmail(value)) {
-          setErrorFor(item, `Not a valid ${itemStr}`);
+        } else if (!isEmail(value)) {
+          setErrorFor(item, 'Not a valid email');
         } else {
           setSuccessFor(item);
         }
         break;
       case 'Password':
-        if(value === '') {
+        if(value.length <= 0) {
           setErrorFor(item, `${itemStr} cannot be blank`);
+        } else {
+          setSuccessFor(item);
         }
-         else {
+        break;
+      case 'PassConfirm':
+        if(value.length <= 0) {
+          setErrorFor(item, 'Password cannot be blank');
+        } else if (value !== pass) {
+          setErrorFor(item, 'Passwords does not match');
+        } else {
           setSuccessFor(item);
         }
         break;
