19a20d7e0e2512bb08b4447ee6c5dcaab118bc39
David Elì <davidelimg1@gmail.com>
2020-06-10 18:00:32 -0500

    Workingon home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @comments = Comment.all
     @users = User.all
+    @posts = current_user.friends_and_own_posts
   end
 
   def about
