9acd4d0537924b40ad575b76ab415b905913572f
David Elì <davidelimg1@gmail.com>
2020-06-10 09:49:09 -0500

    Edited signin, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :posts do
     resources :like_posts, only: %i[create destroy]
     resources :comments, only: %i[create] do
