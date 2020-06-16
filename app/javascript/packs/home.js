505bdf15534bb6f6d170b0b9f8d5c4cc5ed6ad40
David Elì <davidelimg1@gmail.com>
2020-06-15 21:40:08 -0500

    Added sweetalert

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,6 @@
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+
 const NoticeScan =  (() => {
   const noticeText = document.getElementById('notice');
 
@@ -26,6 +29,15 @@ const NoticeScan =  (() => {
 const scanner = NoticeScan;
 
 const start = (() => {
-  scanner.injectNoticeCont();
-  scanner.scan();
+  // scanner.injectNoticeCont();
+  // scanner.scan();
+  Swal.fire({
+    title: 'Sucessfully Login',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  });
 })();
