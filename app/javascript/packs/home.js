43da9986cfc5a306afa227cc5ab48a4e39d0d479
David Elì <davidelimg1@gmail.com>
2020-06-15 20:26:49 -0500

    adding animation to pop ups

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,2 +1,31 @@
-console.log(' hello home!');
-const alertText = document.getElementById('notice');
+const NoticeScan =  (() => {
+  const noticeText = document.getElementById('notice');
+
+  const injectNoticeCont = () => {
+    const noticeBox = document.createElement('div');
+    noticeBox.classList.add('notice-box');
+    noticeBox.id = 'notice-box';
+    noticeBox.classList.add('d-none');
+    noticeBox.appendChild(noticeText);
+    body.prepend(noticeBox);
+  };
+
+  const scan = () => {
+    if(noticeText.innerText != '') {
+      const noticeBox = document.getElementById('notice-box');
+      noticeBox.classList.remove('d-none');
+    }
+  };
+
+  return {
+      injectNoticeCont,
+      scan
+  }
+})();
+
+const scanner = NoticeScan;
+
+const start = (() => {
+  scanner.injectNoticeCont();
+  scanner.scan();
+})();
