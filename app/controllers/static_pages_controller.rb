33fbe3150ac75e98a51a231d3bac14481d9864c3
David Elì <davidelimg1@gmail.com>
2020-06-18 16:01:03 -0500

    Fiex lint issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     return unless user_signed_in?
 
     @post = current_user.posts.build
-    @feed_items = current_user.friends_and_own_posts.sort_by(&:'created_at').reverse
+    @feed_items = current_user.friends_and_own_posts.sort_by(&:created_at).reverse
     @feed_items = @feed_items.paginate(page: params[:page], per_page: 10)
   end
 
