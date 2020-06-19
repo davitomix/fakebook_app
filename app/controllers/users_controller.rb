7a01c7be877d33bae8b9ac9a6c2ea169ce3c20e8
David Elì <davidelimg1@gmail.com>
2020-06-19 01:14:47 -0500

    Fixes show method on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts_items = Post.where(user_id: current_user.id).sort_by(&:created_at).reverse
+    @posts_items = Post.where(user_id: @user.id).sort_by(&:created_at).reverse
     @posts_items = @posts_items.paginate(page: params[:page], per_page: 10)
   end
 end
