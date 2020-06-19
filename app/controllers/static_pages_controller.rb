ffae13b92081941aa5f5c311cf81a2a962674656
David Elì <davidelimg1@gmail.com>
2020-06-18 23:26:37 -0500

    Modified redirect of edit post

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
 
     @post = current_user.posts.build
     @feed_items = current_user.friends_and_own_posts.sort_by(&:created_at).reverse
-    @feed_items = @feed_items.paginate(page: params[:page], per_page: 10)
+    @feed_items = @feed_items.paginate(page: params[:page], per_page: 15)
   end
 
   def about
