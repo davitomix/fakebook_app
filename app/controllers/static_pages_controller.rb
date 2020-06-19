9a05dd429d05ad0ebafdf42187c3c8b461d19e33
David Elì <davidelimg1@gmail.com>
2020-06-19 15:56:34 -0500

    Finished who add section

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     @post = current_user.posts.build
     @feed_items = current_user.friends_and_own_posts.sort_by(&:created_at).reverse
     @feed_items = @feed_items.take(30).paginate(page: params[:feed_items_page], per_page: 10)
-    @who_friends = User.all - current_user.current_friends.take(15)
-    @who_friends = @who_friends.paginate(page: params[:who_friend_page], per_page: 5)
+    @who_friends = User.all - current_user.current_friends.take(25)
+    @who_friends = @who_friends.paginate(page: params[:who_friend_page], per_page: 7)
   end
 
   def about
